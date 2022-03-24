6d293032bad8fd8535047b01408f4f0d2a3483ca
Chew Tzi Hwee <chewtzihwee@gmail.com>
2022-03-24 17:31:27 +0800

    added inventory struct

--- src/types/Inventory.ts
+++ src/types/Inventory.ts
@@ -1,11 +1,27 @@
+import { Timestamp } from 'firebase/firestore'
+
 export default interface InventoryItem {
     id: string,
     name: string,
     parent: string,
     children: string[],
     simpleId: string;
-    metadata: {
-        donatedBy: string,
-        donatedOn: string,
+    metadata?: {
+        donatedBy?: string,
+        donatedOn?: string,
+        serialNumber?: string,
+        model?: string,
+        price?: number,
+        purchasedOn?: string,
+        purchasedFrom?: string,
+        purchasedBy?: {
+            studentid: string,
+            englishName: string,
+        },
+    },
+    registeredBy: {
+        studentid: string,
+        englishName: string,
     }
+    dateRegistered: Timestamp,
 }
