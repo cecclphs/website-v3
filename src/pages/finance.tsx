de4f9b555cd13b59fe2e4fec623917f3926d8b1e
ImJustChew <chewtzihwee@gmail.com>
2022-03-25 22:12:19 +0800

    deploy api

---
+++ src/pages/finance.tsx
@@ -0,0 +1,42 @@
+import { collection } from "firebase/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import MemberLayout from "../components/MemberLayout";
+import Page from "../components/Page";
+import { db, docConverter } from "../config/firebase";
+import { Transaction } from "../types/Finance";
+
+const FinanceRow = ({transaction}: { transaction: Transaction }) => {
+    const { id, type, amount, description, date } = transaction;
+    return <tr>
+        <td className='text-sm font-semibold py-2 w-[200px]'>{id}</td>
+        <td>{type}</td>
+        <td>{amount}</td>
+        <td>{description}</td>
+        <td>{date.toLocaleString()}</td>
+    </tr>
+}
+
+const Finance = () => {
+    const [transactions, loading, error] = useCollectionData(collection(db, 'transactions').withConverter(docConverter));
+    console.log(transactions)
+    return <MemberLayout>
+        <Page title="Financials">
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Date</th>
+                        <th>Description</th>
+                        <th></th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </Page>
+    </MemberLayout>
+}
+
+export default Finance;
